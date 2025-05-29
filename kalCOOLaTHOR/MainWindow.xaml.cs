20448321170283d2552bf70bd4cd94aa56ab789f
Екатерина Омельницкая <k625972@gmail.com>
2025-05-29 11:12:26 +0500

    -

--- kalCOOLaTHOR/MainWindow.xaml.cs
+++ kalCOOLaTHOR/MainWindow.xaml.cs
@@ -20,9 +20,146 @@ namespace kalCOOLaTHOR
     /// </summary>
     public partial class MainWindow : Window
     {
+        private double lastNumber;
+        private string selectedOperator;
+        private bool isNewNumber;
+
         public MainWindow()
         {
             InitializeComponent();
+            ResetCalculator();
+        }
+
+        private void ResetCalculator()
+        {
+            lastNumber = 0;
+            selectedOperator = "";
+            isNewNumber = true;
+            Display.Text = "0";
+        }
+
+        private void Number_Click(object sender, RoutedEventArgs e)
+        {
+            Button button = (Button)sender;
+            string number = button.Content.ToString();
+
+            if (Display.Text == "0" || isNewNumber)
+            {
+                Display.Text = number;
+                isNewNumber = false;
+            }
+            else
+            {
+                Display.Text += number;
+            }
+        }
+
+        private void Operation_Click(object sender, RoutedEventArgs e)
+        {
+            Button button = (Button)sender;
+            selectedOperator = button.Content.ToString();
+            lastNumber = double.Parse(Display.Text);
+            isNewNumber = true;
+        }
+
+        private void BtnEquals_Click(object sender, RoutedEventArgs e)
+        {
+            if (string.IsNullOrEmpty(selectedOperator))
+                return;
+
+            double newNumber = double.Parse(Display.Text);
+            double result = 0;
+
+            switch (selectedOperator)
+            {
+                case "+":
+                    result = lastNumber + newNumber;
+                    break;
+                case "-":
+                    result = lastNumber - newNumber;
+                    break;
+                case "*":
+                    result = lastNumber * newNumber;
+                    break;
+                case "/":
+                    if (newNumber == 0)
+                    {
+                        MessageBox.Show("Деление на ноль невозможно!", "Ошибка", MessageBoxButton.OK, MessageBoxImage.Error);
+                        ResetCalculator();
+                        return;
+                    }
+                    result = lastNumber / newNumber;
+                    break;
+                case "^":
+                    result = Math.Pow(lastNumber, newNumber);
+                    break;
+            }
+
+            Display.Text = result.ToString();
+            selectedOperator = "";
+            isNewNumber = true;
+            lastNumber = result;
+        }
+
+        private void BtnDecimal_Click(object sender, RoutedEventArgs e)
+        {
+            if (isNewNumber)
+            {
+                Display.Text = "0,";
+                isNewNumber = false;
+                return;
+            }
+
+            if (!Display.Text.Contains(","))
+            {
+                Display.Text += ",";
+            }
+        }
+
+        private void BtnClear_Click(object sender, RoutedEventArgs e)
+        {
+            ResetCalculator();
+        }
+
+        private void BtnBackspace_Click(object sender, RoutedEventArgs e)
+        {
+            if (Display.Text.Length == 1 || (Display.Text.Length == 2 && Display.Text.StartsWith("-")))
+            {
+                Display.Text = "0";
+            }
+            else
+            {
+                Display.Text = Display.Text.Substring(0, Display.Text.Length - 1);
+            }
+        }
+
+        private void BtnSqrt_Click(object sender, RoutedEventArgs e)
+        {
+            double number = double.Parse(Display.Text);
+            if (number < 0)
+            {
+                MessageBox.Show("Нельзя извлечь корень из отрицательного числа!", "Ошибка", MessageBoxButton.OK, MessageBoxImage.Error);
+                return;
+            }
+
+            double result = Math.Sqrt(number);
+            Display.Text = result.ToString();
+            isNewNumber = true;
+        }
+
+        private void BtnPlusMinus_Click(object sender, RoutedEventArgs e)
+        {
+            if (Display.Text == "0")
+                return;
+
+            if (Display.Text.StartsWith("-"))
+            {
+                Display.Text = Display.Text.Substring(1);
+            }
+            else
+            {
+                Display.Text = "-" + Display.Text;
+            }
         }
     }
 }
